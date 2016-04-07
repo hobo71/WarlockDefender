ce8130c940dc18b9555d6431696691ab09c2c1a5
alarco-l <ludovic.alarcon@epitech.eu>
2016-04-07 10:40:51 +0900

    toto

---
+++ Assets/Scripts/TryPathIsOk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TryPathIsOk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    NavMeshPath path = new NavMeshPath();
+        GetComponent<NavMeshAgent>().CalculatePath(destination, path);
+        if (path.status == NavMeshPathStatus.PathPartial)
+	        Console.Log()
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
