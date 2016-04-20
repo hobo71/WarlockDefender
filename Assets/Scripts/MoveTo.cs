3ca209556ba105e23a4586ff9438bde2977c00af
alarco-l <ludovic.alarcon@epitech.eu>
2016-04-21 01:01:07 +0900

    AI tower

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -1,12 +1,38 @@
-﻿    using UnityEngine;
-    using System.Collections;
-    
-    public class MoveTo : MonoBehaviour {
-       
-       public Transform goal;
-       
-       void Start () {
-          NavMeshAgent agent = GetComponent<NavMeshAgent>();
-          agent.destination = goal.position; 
-       }
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int index = 0;
+    private NavMeshAgent agent;
+    private int approxX, approxY;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        ToNextPoint();
     }
+
+    public void ToNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        if (index == points.Length)
+        {
+            agent.autoBraking = true;
+            return;
+        }
+        Vector3 pos = new Vector3(points[index].position.x + Random.Range(-20,20), points[index].position.y + Random.Range(-20, 20), points[index].position.z);
+        agent.destination = pos;
+        ++index;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.5f)
+            ToNextPoint();
+    }
+}
---
+++ Assets/Scripts/TowerAttack.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerAttack : MonoBehaviour {
+    public int damage = 10;
+    public float attackSpeed = 0.5f;
+    private List<GameObject> _enemies;
+    private EnemieStats enemie;
+    internal bool _update;
+    internal float time;
+
+	// Use this for initialization
+	void Start () {
+        _enemies = new List<GameObject>();
+        _update = true;
+        time = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_enemies.Count == 0)
+            return;
+        time += Time.deltaTime;
+        if (enemie == null && !_update)
+        {
+            _update = true;
+            _enemies.RemoveAt(0);
+        }
+        if (_update)
+        {
+            if (_enemies.Count > 0)
+            {
+                enemie = _enemies[0].GetComponent<EnemieStats>();
+                _update = false;
+            }
+        }
+        if (time >= attackSpeed)
+        {
+            enemie.ApplyDamage(damage);
+            time = 0.0f;
+        }
+ 	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemie")
+        {
+            _enemies.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _enemies.Remove(other.gameObject);
+    }
+}
